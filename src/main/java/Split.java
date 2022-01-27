290518d853e0910704e83a20a132d47777d8be08
EdgarRafaelRomeroMartin <a220211709@unison.mx>
2022-01-26 18:14:25 -0700

    Segunda tarea

---
+++ src/main/java/Split.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Split {
+
+    public static void main(String args[]) {
+
+        if (args.length == 0) {
+            System.out.println("Falta el nombre del archivo");
+            System.exit(1);
+        }
+
+        String fileName = args[0];
+
+        FileReader fileReader = null;
+
+        try {
+            fileReader = new FileReader(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("El nombre del archivo no se encuentra");
+            System.exit(2);
+        }
+
+        BufferedReader in = new BufferedReader(fileReader);
+
+
+        String UwU= null;
+
+        String []palabras;
+
+        int contadorPalabras=0;
+        long start = System.currentTimeMillis();
+        try {
+            while ((UwU = in.readLine()) != null) {
+                palabras = UwU.split("\\s+");
+                for(String a : palabras) {
+                    contadorPalabras++;
+                }
+                }
+
+
+        } catch (IOException e) {
+            System.out.println("Error");
+            System.exit(3);
+        }
+
+
+
+        long time = System.currentTimeMillis() - start;
+        try {
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("El archivo " + fileName + " tiene " + contadorPalabras );
+        System.out.println( "Palabras y " + "el progrma duro "+ time +" ms en correr");
+
+    }
+
+}
+
